2ccf60b1312f29091b3f3ea00b1332f3ae6f6d19
corecaps <corecaps@gmail.com>
2022-07-28 17:19:42 +0200

    a

--- config.h
+++ config.h
@@ -6,14 +6,14 @@
 /*   By: jgarcia <jgarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 12:39:18 by jgarcia           #+#    #+#             */
-/*   Updated: 2022/07/27 12:39:22 by jgarcia          ###   ########.fr       */
+/*   Updated: 2022/07/28 16:33:18 by jgarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_CONFIG_H
 #define FRACTOL_CONFIG_H
-# define DEFAULT_X 600
-# define DEFAULT_Y 600
+# define DEFAULT_X 1024
+# define DEFAULT_Y 1024
 # define MAX_ITER 60
 # define KEY_UP 65362
 # define KEY_DOWN 65364
